448d09a6aff009287faeed77927c77042c6bcf4b
ImEagleEyE <faisalaminanik786786@gmail.com>
2026-05-07 16:33:28 +0600

    improved

--- js/script.js
+++ js/script.js
@@ -8,22 +8,45 @@ let chance=document.querySelector("#chance");
 let gameover=document.querySelector("#gameover");
 let wrong=document.querySelector("#wrong");
 let congrats=document.querySelector("#congrats");
-let first= num1.value;
-let second= num2.value;
+let reveal=document.querySelector("#reveal");
+let revealed=document.querySelector("#revealed");
+let error1=document.querySelector("#error1");
+let error2=document.querySelector("#error2");
+let first= 0;
 start.addEventListener("click",()=>{
-    player2.style.display="block";
-    reset.style.display="inline-block";
-    chance.textContent="3";
-    check.style.display = "inline-block";
-    gameover.style.display = "none";
+    if (num1.value<0 || num1.value>10|| isNaN(num1.value)) {
+    error1.innerHTML = "Please enter a number between 0 and 10.";
+    num1.value = "";
+    }else{
+        player2.style.display="block";
+        reset.style.display="inline-block";
+        chance.textContent="3";
+        check.style.display = "inline-block";
+        gameover.style.display = "none";
+        first= num1.value;
+        num1.value="";
+        start.style.display="none";
+        error1.innerHTML ="";
+    }
 })
 reset.addEventListener("click",()=>{
     player2.style.display="none";
+    congrats.style.display = "none";
+    wrong.style.display = "none";
+    start.style.display="inline-block";
+    reset.style.display="none";
 })
 check.addEventListener("click",()=>{
-    if(num1.value==num2.value){
+    if(num2.value<0 || num2.value>10|| isNaN(num2.value)) {
+        error2.innerHTML = "Please enter a number between 0 and 10.";
+        num2.value = "";
+        return;
+    }else{
+        error2.innerHTML = "";
+        if(first==num2.value){
         congrats.style.display = "block";
         wrong.style.display = "none";
+        check.style.display = "none";
     }else{
         let chancesLeft = parseInt(chance.textContent) - 1;
         chance.textContent = chancesLeft;
@@ -33,7 +56,16 @@ check.addEventListener("click",()=>{
             chance.textContent = "0";
             check.style.display = "none";
             wrong.style.display = "none";
+            reveal.style.display = "inline-block";
         }
+    num2.value="";
     }
+    }
+    
 });
+reveal.addEventListener("click",()=>{
+
+    revealed.innerHTML = `The number was: ${first}`;
+});
+
 
